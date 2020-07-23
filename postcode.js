ae0bd38528f8843addd4ac85c8b3fc5135b4026e
Joshua <joshua.hart@protonmail.com>
2020-07-23 11:26:09 +0100

    Edit output to be object
    Object includes District, County, Country, Longitude, Latitude

--- postcode.js
+++ postcode.js
@@ -1,17 +1,24 @@
 
-let inputPostcode = document.querySelector('#postcode');
-let district;
+let inputPostcode = document.querySelector('#postcode'); // targetting when a postcode in the UK
+let postcodeInfo = { } // creating object for GET method of postcode information
 const form = document.querySelector('form');
 form.addEventListener("submit", event => {
     event.preventDefault();
-    fetch(`https://api.postcodes.io/postcodes/${inputPostcode.value}`)
+    fetch(`https://api.postcodes.io/postcodes/${inputPostcode.value}`) // fetch for postcode information to then access covid and weather data
         .then(response => {
             if (!response.ok) throw new Error(response.status);
             return response.json();
         })
         .then(response => {
-            district = response.result['admin_district'];
-            return district;
+            postcodeInfo = {
+                district:  response.result['admin_district'],
+                county: response.result['admin_county'],
+                country: response.result['country'],
+                longitude: response.result['longitude'],
+                latitude: response.result['latitude']
+            }
+            console.log(postcodeInfo);       
+            return postcodeInfo;
         })
         .catch(error => {
             if (error.message === "404") {
