5fd423965cc283cf3322da444ac21cd065333b33
Amber <a.rignell16@ejm.org>
2020-07-23 17:43:03 +0200

    insert display results into weather js and covid js
    Relates #14

--- weather.js
+++ weather.js
@@ -1,13 +1,16 @@
 let weatherInfo = {};
 
 const getWeatherData = (postcodeInfo) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${postcodeInfo.latitude}&lon=${postcodeInfo.longitude}&units=metric&appid=c3f0332f8bfedec6bf95c9eaaa1c6d36`)  
-    .then(response => response.json())
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${postcodeInfo.latitude}&lon=${postcodeInfo.longitude}&units=metric&appid=c3f0332f8bfedec6bf95c9eaaa1c6d36`)  // reques data from WEATHER API
+    .then(response => response.json())  // access JSON object
     .then(json => {
         weatherInfo = {
-             description: json.weather[0].description,
-             temperature: json.main.temp
+             description: json.weather[0].icon,  // target property related to weather description
+             temperature: json.main.temp    // target property related to temperature data
         }
+        displayResultsWeather.innerText = `It's ${weatherInfo.description} today and the temperature is ${weatherInfo.temperature}`; //Insert updated weather text into the paragraph tag
+        const imgSrc = weatherInfo.description.replace(" ", "-");
+        displayResultsImage.src = `./images/${imgSrc}.svg`;
     })
     .catch(error => console.log(error));
 }
