8eb42c4a376ceaca6b616704b324c6f51803174b
LiCern <lisacernilogar@me.com>
2020-07-23 16:03:03 +0200

    Get description and temperature variables
    Relates #13

--- weather.js
+++ weather.js
@@ -1,9 +1,16 @@
+let weatherInfo = {};
+
 const getWeatherData = (postcodeInfo) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${postcodeInfo.latitude}&lon=${postcodeInfo.longitude}&appid=c3f0332f8bfedec6bf95c9eaaa1c6d36`)
-    .then(response => console.log(response.json()))
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${postcodeInfo.latitude}&lon=${postcodeInfo.longitude}&units=metric&appid=c3f0332f8bfedec6bf95c9eaaa1c6d36`)  
+    .then(response => response.json())
+    .then(json => {
+        weatherInfo = {
+             description: json.weather[0].description,
+             temperature: json.main.temp
+        }
+    })
     .catch(error => console.log(error));
 }
-
 
 
 
